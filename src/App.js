8441bfcdf5a3959d16595b6152bef92d113c5ff8
Collin Monahan <llaenowyd@gmail.com>
2021-03-23 17:06:48 -0400

    device / viewport toy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {makeStyles} from '@material-ui/core'
+import useMediaQuery from '@material-ui/core/useMediaQuery'
+import { useTheme } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+
+const useStyles = makeStyles({
+  app: {
+    backgroundColor: 'darkBlue',
+    bottom: 0,
+    color: 'white',
+    display: 'flex',
+    flexDirection: 'row',
+    fontSize: '4vw',
+    fontWeight: 700,
+    left: 0,
+    position: 'absolute',
+    right: 0,
+    top: 0,
+  },
+  fauxContent: {
+    backgroundColor: 'lightcoral',
+    display: 'flex',
+    flexDirection: 'column',
+    marginLeft: 'auto',
+    flex: 1,
+    padding: '1vw',
+  },
+  fauxSidenav: {
+    backgroundColor: 'lightcyan',
+    color: 'darkblue',
+    display: 'flex',
+    flexDirection: 'column',
+    marginRight: '1vw',
+    padding: '1vw',
+    width: '17%',
+  },
+  row: {
+    paddingBottom: '0.5vw',
+  },
+})
+
+const useWindowSize = () => {
+  const [size, setSize] = React.useState([0, 0]);
+  React.useLayoutEffect(() => {
+    const updateSize = () => setSize([window.innerWidth, window.innerHeight])
+
+    window.addEventListener('resize', updateSize)
+    updateSize()
+    return () => window.removeEventListener('resize', updateSize)
+  }, []);
+  return size
 }
 
-export default App;
+const Row = props => {
+  const classes = useStyles()
+
+  return (<div className={classes.row}>{props.children}</div>)
+}
+
+const App = () => {
+  const theme = useTheme()
+
+  const isXS = useMediaQuery(theme.breakpoints.only('xs'))
+  const isSM = useMediaQuery(theme.breakpoints.only('sm'))
+  const isMD = useMediaQuery(theme.breakpoints.only('md'))
+  const isLG = useMediaQuery(theme.breakpoints.only('lg'))
+  const isXL = useMediaQuery(theme.breakpoints.only('xl'))
+
+  const viewportDimensions = useWindowSize()
+
+  const classes = useStyles()
+
+  const breakpointName = isXS ? 'xs' : isSM ? 'sm' : isMD ? 'md' : isLG ? 'lg' : 'xl'
+  const breakpointDescription = {
+    xs: 'extra-small',
+    sm: 'small',
+    md: 'medium',
+    lg: 'large',
+    xl: 'extra-large'
+  }[breakpointName]
+
+  return (
+    <CssBaseline>
+      <div className={classes.app}>
+        <div className={classes.fauxSidenav}>
+          faux side-nav
+        </div>
+        <div className={classes.fauxContent}>
+          <Row>faux content</Row>
+          <Row>{`breakpoint: ${breakpointName} (${breakpointDescription})`}</Row>
+          <Row>{`viewport width: ${viewportDimensions?.[0]}`}</Row>
+          <Row>{`device pixel ratio: ${window.devicePixelRatio}`}</Row>
+        </div>
+      </div>
+    </CssBaseline>
+  )
+}
+
+export default App
